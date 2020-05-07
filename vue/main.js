7a27ad6199729e1e2d8dc4559c95850b0d9c0171
ropomkopo <roman.lysenko@hebronsoft.com>
2020-05-07 17:00:43 +0300

    First Step

---
+++ vue/main.js
@@ -0,0 +1,12 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        title: 'hello, you are in lol chat',
+        product: 'Socks',
+        name: '',
+        rooms: ['politikal-people', 'admin', 'simple-people']
+    },
+    methods: {
+        
+    }
+})
